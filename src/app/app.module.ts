f28008bae8b0211873e4c2e258691d2c83277042
kevinruse <kevin@kevinruse.com>
2020-07-15 09:58:25 -0700

    using 3rd-party libraries and advanced styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ChildComponent } from './child/child.component';
+import { FormInputComponent } from './form-input/form-input.component';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-    imports:      [ BrowserModule ],
+    imports:      [ BrowserModule, FontAwesomeModule ],
     declarations: [ AppComponent,
                     HeaderComponent,
-                    ChildComponent ],
+                    ChildComponent,
+                    FormInputComponent ],
     bootstrap:    [ AppComponent ]
 })
 
