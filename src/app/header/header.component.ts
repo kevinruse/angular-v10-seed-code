767cc906cf72c3ed995d87a8b1af18b3eaf9aaad
kevinruse <kevin@kevinruse.com>
2020-07-14 19:20:33 -0700

    challenge exercise: Using the templateUrl property

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
-    template: `<header>
-		<img src='assets/images/seed-icon.png'>
-                 <h1>Welcome to Angular Seed!</h1>
-               </header>`
+    templateUrl: './header.component.html'
 })
 
 export class HeaderComponent {
