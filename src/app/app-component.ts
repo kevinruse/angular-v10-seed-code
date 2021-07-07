3aa561833e21c7689f05061e462ee2c49e5d45ed
Kevin Ruse <kevin@kevinruse.com>
2021-07-07 09:23:07 -0700

    rename app component

--- src/app/app-component.ts
+++ src/app/app-component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app-component.html',
+  styleUrls: ['./app-component.css']
 })
 export class AppComponent {
-  title = 'angular-v9-seed';
+  title = 'angular-v10-seed';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AppComponentComponent } from './app-component/app-component.component';
 
 @NgModule({
     imports:      [ BrowserModule ],
-    declarations: [ AppComponent, AppComponentComponent ],
+    declarations: [ AppComponent ],
     bootstrap:    [ AppComponent ]
 })
 
