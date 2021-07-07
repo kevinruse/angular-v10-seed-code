f4089646d0fa8775fe0e394e528016ab398819cb
Kevin Ruse <kevin@kevinruse.com>
2021-07-07 08:39:18 -0700

    restore app.component to its original state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppComponentComponent } from './app-component/app-component.component';
 
 @NgModule({
     imports:      [ BrowserModule ],
-    declarations: [ AppComponent ],
+    declarations: [ AppComponent, AppComponentComponent ],
     bootstrap:    [ AppComponent ]
 })
 
